193ee95f2324164e5b7dfbab35f7a289eafc9273
Bayological <6872903+bayological@users.noreply.github.com>
2023-09-18 12:17:40 -0500

    refactor(ccxt): clean up adapter functions

--- packages/adapter-ccxt/src/CCXTAdapter.ts
+++ packages/adapter-ccxt/src/CCXTAdapter.ts
@@ -1,11 +1,5 @@
 import { autoInjectable } from "tsyringe";
-import {
-   IAdapter,
-   IConfigProvider,
-   Step,
-   AdapterConfig,
-   ValidationResult,
-} from "@mate/sdk";
+import { IAdapter, IConfigProvider, Step, ValidationResult } from "@mate/sdk";
 import { ExecutionResult } from "./ExecutionResult";
 import { IValidator } from "./validation";
 import { CCXTAdapterConfig } from "./CCXTAdapterConfig";
@@ -30,10 +24,12 @@ export class CCXTAdapter implements IAdapter<ExecutionResult, CCXTStep> {
       private exchangeServiceRepo: IExchangeServiceRepo
    ) {}
 
-   public async init(): Promise<Boolean> {
-      const config: AdapterConfig | null = this.configProvider.getAdapterConfig(
-         this.adapterId
-      );
+   public async init(): Promise<boolean> {
+      if (this.adapterConfig) {
+         return true; // Already initialized
+      }
+
+      const config = this.configProvider.getAdapterConfig(this.adapterId);
 
       if (!config) {
          throw new Error(
@@ -45,49 +41,26 @@ export class CCXTAdapter implements IAdapter<ExecutionResult, CCXTStep> {
          this.adapterConfig = await this.adapterConfigValidator.validate(
             config
          );
-
-         // for each exchange in the config, initialize the exchange then.
-         for (const [exchangeId, exchangeConfig] of Object.entries(
-            this.adapterConfig.exchanges
-         ) as Array<[ExchangeId, ApiCredentials]>) {
-            const exchangeService = this.exchangeFactory.createExchangeService(
-               exchangeId,
-               exchangeConfig
-            );
-
-            // Add the exchange service to the repo to allow for retrieval later in other steps or parts of the adapter.
-            this.exchangeServiceRepo.setExchangeService(
-               exchangeId,
-               exchangeService
-            );
-         }
+         await this.initializeExchanges();
       } catch (err: any) {
          throw new Error(`${ERR_ADAPTER_INIT_FAILURE}: ${err.message}`);
       }
 
-      return Promise.resolve(true);
+      return true;
    }
 
-   //TODO: CCXTStep type is not correct.
-   //TODO: Refactor, do not need to prefix everyting with CCXT.
-
-   public isValid(step: Step<CCXTStep>): ValidationResult {
+   public async isValid(step: Step<CCXTStep>): Promise<ValidationResult> {
       let result: ValidationResult = {
          isValid: false,
          errors: [],
       };
 
       try {
-         this.stepConfigValidator.validate(step);
+         await this.stepConfigValidator.validate(step);
+         result.isValid = true;
       } catch (err) {
-         result.errors?.push((err as Error).message);
-         result.isValid;
-
-         return result;
+         result.errors.push((err as Error).message);
       }
-
-      // If the validator does not throw an error then the step is valid.
-      result.isValid = true;
 
       return result;
    }
@@ -95,5 +68,20 @@ export class CCXTAdapter implements IAdapter<ExecutionResult, CCXTStep> {
    public execute(step: Step<CCXTStep>): Promise<ExecutionResult> {
       console.log(step);
       throw new Error("Method not implemented.");
+   }
+
+   private async initializeExchanges(): Promise<void> {
+      for (const [exchangeId, exchangeConfig] of Object.entries(
+         this.adapterConfig.exchanges
+      ) as Array<[ExchangeId, ApiCredentials]>) {
+         const exchangeService = this.exchangeFactory.createExchangeService(
+            exchangeId,
+            exchangeConfig
+         );
+         this.exchangeServiceRepo.setExchangeService(
+            exchangeId,
+            exchangeService
+         );
+      }
    }
 }
