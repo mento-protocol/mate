b304adb34b8ead77b766d8120368455b3ac84dbb
Bayological <6872903+bayological@users.noreply.github.com>
2023-09-20 19:40:08 -0500

    test(ccxt): add integration test for binance

---
+++ packages/adapter-ccxt/test/integration/exchanges/BinanceApiService.integration.test.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import "dotenv/config";
+import { binance } from "ccxt";
+import { BinanceApiService } from "../../../src/exchanges";
+
+describe("BinanceApiService Integration", () => {
+   let testee: BinanceApiService;
+   let exchange: binance;
+
+   beforeAll(() => {
+      exchange = new binance({
+         apiKey: process.env.BINANCE_API_KEY,
+         secret: process.env.BINANCE_API_SECRET,
+      });
+
+      exchange.setSandboxMode(true);
+
+      testee = new BinanceApiService(exchange);
+   });
+
+   describe("isAssetSupported", () => {
+      it("should return true if the asset is supported", async () => {
+         const result = await testee.isAssetSupported("BTC");
+         expect(result).toBe(true);
+      });
+
+      it("should return false if the asset is not supported", async () => {
+         const result = await testee.isAssetSupported("NON_EXISTENT_ASSET");
+         expect(result).toBe(false);
+      });
+   });
+
+   describe("getCurrencyBalance", () => {
+      it("should return the balance for BTC", async () => {
+         const result = await testee.getCurrencyBalance("BTC");
+         expect(result).toBeGreaterThan(0);
+      });
+   });
+});
