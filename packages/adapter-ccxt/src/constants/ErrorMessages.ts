4a2a662d4a523a8cd6e85b5f64a504ed7fa75115
Bayological <6872903+bayological@users.noreply.github.com>
2023-09-29 18:18:56 -0500

    refactor(ccxt): clarify missing exchange service

--- packages/adapter-ccxt/src/constants/ErrorMessages.ts
+++ packages/adapter-ccxt/src/constants/ErrorMessages.ts
@@ -10,7 +10,7 @@ export const ERR_API_BALANCE_FETCH_FAILURE = (currency: string) =>
    `Failed to fetch balance for currency: ${currency}`;
 export const ERR_BALANCE_NOT_FOUND = "Currency balance was not found";
 export const ERR_EXCHANGE_SERVICE_NOT_FOUND = (exchange: string) =>
-   `Service for exchange "${exchange}" not found. It might not have been initialized or properly registered in repo.`;
+   `Service for exchange "${exchange}" not found. Verify the configuration for the ${exchange} exchange has been added to the config file.`;
 export const ERR_API_FETCH_MARKETS_FAILURE = (exchange: string) =>
    `Failed to fetch markets for exchange ${exchange}`;
 export const ERR_INVALID_ADDRESS = (address: string, prop: string) =>
