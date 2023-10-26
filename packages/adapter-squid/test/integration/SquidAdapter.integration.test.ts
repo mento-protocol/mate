8ee4da963896a60c75711c220e7650980c854da4
Bayological <6872903+bayological@users.noreply.github.com>
2023-10-26 16:51:28 +0100

    test(squid): add integration test

---
+++ packages/adapter-squid/test/integration/SquidAdapter.integration.test.ts
@@ -0,0 +1,87 @@
+import "reflect-metadata";
+import * as path from "path";
+import pkg from "../../package.json";
+import { DependencyRegistrar } from "../../src/DependencyRegistrar";
+import {
+   ConfigProvider,
+   IConfigProvider,
+   DependencyRegistrar as SdkDependencyRegistrar,
+} from "@mate/sdk";
+import { SquidAdapter } from "../../src/SquidAdapter";
+import { container } from "tsyringe";
+
+describe("SquidAdapter Integration", () => {
+   let adapter: SquidAdapter;
+   let configProvider: IConfigProvider;
+
+   beforeAll(() => {
+      // Override the config file path to use the example config
+      process.env["CONFIG_PATH"] = path.resolve(
+         __dirname,
+         "../../../../config.example.yaml"
+      );
+
+      // Configure the global container
+      SdkDependencyRegistrar.configure();
+      DependencyRegistrar.configure();
+
+      // Get an instance of the adapter from the container
+      adapter = container.resolve(pkg.name);
+
+      // Get an instance of the config provider from the container
+      configProvider = container.resolve(ConfigProvider);
+   });
+
+   describe("Init", () => {
+      it("should initialize the adapter", async () => {
+         expect(await adapter.init()).toBe(true);
+      });
+   });
+
+   describe("IsValid", () => {
+      beforeAll(async () => {
+         await adapter.init();
+      });
+
+      it("should return true for a valid step", async () => {
+         // Get a valid step from the config
+         const step = configProvider.getStepFromFlow("axlusd-to-usd", 0);
+
+         // Validate the step
+         const result = await adapter.isValid(step);
+
+         // Check the result
+         expect(result.isValid).toBe(true);
+      });
+
+      it("should return false for an invalid step", async () => {
+         // Get an invalid step from the config
+         const step = configProvider.getStepFromFlow("axlusd-to-usd", 1);
+
+         // Validate the step
+         const result = await adapter.isValid(step);
+
+         // Check the result
+         expect(result.isValid).toBe(false);
+      });
+   });
+
+   describe("Execute", () => {
+      beforeAll(async () => {
+         await adapter.init();
+      });
+
+      it("should execute a valid step", async () => {
+         // Get a valid step from the config
+         const step = configProvider.getStepFromFlow("axlusd-to-usd", 0);
+
+         // Execute the step
+         const result = await adapter.execute(step);
+         console.log(result);
+
+         // Check the result
+         expect(result).toBeDefined();
+         expect(result.success).toBe(true);
+      });
+   });
+});
