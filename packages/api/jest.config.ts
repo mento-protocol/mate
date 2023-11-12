3ab7faab31d41f0ee46181a38a14d9d09e9b6c29
Bayological <6872903+bayological@users.noreply.github.com>
2023-11-12 10:57:30 -0500

    feat(api): add basic api server

---
+++ packages/api/jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+   preset: "ts-jest",
+   testEnvironment: "node",
+   roots: ["<rootDir>/test/"],
+   transform: {
+      "^.+\\.tsx?$": "ts-jest",
+   },
+   testRegex: "test/.*\\.test\\.ts$",
+   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+   collectCoverage: true,
+   collectCoverageFrom: [
+      "src/**/*.ts",
+      "!src/types/**/*",
+      "!src/constants/**/*",
+      "!src/**/index.ts",
+   ],
+   coverageDirectory: "<rootDir>/coverage/",
+   coveragePathIgnorePatterns: ["index.ts, src/constants, src/types"],
+   testTimeout: 20000,
+};
+
+export default config;
---
+++ packages/api/src/app.ts
@@ -0,0 +1,68 @@
+import fastify from "fastify";
+import { bootstrap, Engine } from "@mate/engine";
+
+const port = Number(process.env.API_PORT) || 8080;
+
+// TODO: Basic auth.
+
+async function startServer() {
+   try {
+      const server = fastify({ logger: true });
+      let engine: Engine;
+
+      // Initialize the M.A.T.E engine
+      try {
+         engine = await bootstrap();
+      } catch (bootstrapError) {
+         console.error("Failed to initialize M.A.T.E engine:", bootstrapError);
+         process.exit(1);
+      }
+
+      // Health Check route
+      server.get("/health", async () => {
+         return { status: "OK" };
+      });
+
+      // Execute flow endpoint
+      server.post<{ Params: { flowId: string } }>(
+         "/v1/execute/:flowId",
+         async (request, reply) => {
+            const flowId = request.params.flowId;
+
+            try {
+               const executionResult = await engine.execute(flowId);
+               return executionResult;
+            } catch (error) {
+               let errorMessage;
+               if (error instanceof Error) {
+                  errorMessage = error.message;
+               } else {
+                  errorMessage = "Unknown error";
+               }
+               return reply.code(500).send({
+                  error: `Execution of flow '${flowId}' failed: ${errorMessage}`,
+               });
+            }
+         }
+      );
+
+      // Start the server
+      server.listen({ port }, (err, address) => {
+         if (err) {
+            console.error(err);
+            process.exit(1);
+         }
+         console.log(`Server listening at ${address}`);
+      });
+   } catch (error) {
+      console.error("Server failed to start:", error);
+      process.exit(1);
+   }
+}
+
+process.on("unhandledRejection", (error) => {
+   console.error("Unhandled Rejection:", error);
+   process.exit(1);
+});
+
+startServer();
